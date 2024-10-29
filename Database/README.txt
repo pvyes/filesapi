7442b9a11ca60c8107c3d6074cdb7df4e38eedc5
PVYes <vs_peter@hotmail.com>
2024-10-30 00:10:48 +0100

    trying to meet db and model

--- Database/README.txt
+++ Database/README.txt
@@ -1,5 +1,4 @@
 db.js: Initializes Sequelize with MySQL connection details.
 models.js: Defines the models and their relationships.
-sync.js: Syncs the models with the database, creating the necessary tables.
 
-run 'node sync.js' to sync the models
+database.js: all functions
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 require("dotenv").config();
 
 const db = require("./Database/database");
+const Tag = require("./Model/Tag");
 
-db.createDb();
+testdb();
+
+async function testdb() {
+  var dbtags = await db.getAllTags();
+  //console.log("DbTag:", dbtags);
+  var tags = [];
+  dbtags.forEach((tag) => {
+    var newtag = new Tag();
+    newtag.setId(tag.tid);
+    newtag.setTagname(tag.tagname);
+    newtag.setCreatedAt(tag.createdAt);
+    newtag.setUpdatedAt(tag.updatedAt);
+    tags.push(newtag);
+  });
+  console.log("Tag 3 :", tags[2].getTagname());
+  tags[2].setTagname("Comedy");
+  await Tag.writeTagToDb(tags[2]);
+  console.log("Tag written 2:", tags[2].getId());
+}
