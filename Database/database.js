1d80080c9ac4ad355ef55a5ef78a66058b5c8da3
PVYes <vs_peter@hotmail.com>
2024-10-30 12:30:49 +0100

    Databasemodel with example records

--- Database/database.js
+++ Database/database.js
@@ -7,9 +7,9 @@ const {
   Artist,
 } = require("./models");
 
-const createDb = function () {
+const createDb = async function () {
   try {
-    sequelize.sync({ force: true }).then(() => {
+    await sequelize.sync({ force: true }).then(() => {
       console.log("Database & tables created.");
     });
   } catch (e) {
@@ -17,9 +17,9 @@ const createDb = function () {
   }
 };
 
-const updateDb = function () {
+const updateDb = async function () {
   try {
-    sequelize.sync({}).then(() => {
+    await sequelize.sync({}).then(() => {
       console.log("Database & tables updated.");
     });
   } catch (e) {
@@ -29,7 +29,7 @@ const updateDb = function () {
 
 //CREATE
 
-// Create a new File
+/* Create a new File
 async function createFile(file) {
   const newFile = await File.create({
     uri: file.getUri(),
@@ -37,16 +37,40 @@ async function createFile(file) {
     description: file.getDescription(),
     reference: file.getReference(),
   });
-  await newFile.addTags(file.getTags());
-  await newFile.addArtists(file.getArtists());
+  file.getTags().forEach(async (tag) => {
+    var dbtag = await getTagById(tag.getId());
+    await newFile.addTag(dbtag);
+  });
+  console.log("File created:", newFile);
+  return newFile;
+}
+*/
+// Create a new File
+async function createFile(file) {
+  const newFile = await File.create({
+    uri: file.uri,
+    title: file.title,
+    description: file.description,
+    reference: file.reference,
+  });
   console.log("File created:", newFile);
   return newFile;
 }
 
-// Create a new Tag
+/* Create a new Tag
 async function createTag(tag) {
   const newTag = await Tag.create({
     tagname: tag.getTagname(),
+  });
+  console.log("Tag created:", newTag);
+  return newTag;
+}
+*/
+
+// Create a new Tag
+async function createTag(tag) {
+  const newTag = await Tag.create({
+    tagname: tag.tagname,
   });
   console.log("Tag created:", newTag);
   return newTag;
@@ -74,42 +98,60 @@ async function createArtistFunction(artistfunction) {
   return newArtistFunction;
 }
 
+// Create a new Artist
+async function createArtist(person, artistfunction, file = null) {
+  const newArtist = await Artist.create({
+    person_id: person.aid,
+    artistfunction_id: artistfunction.afid,
+  });
+  if (file != null) {
+    await file.addArtist(newArtist);
+  }
+  console.log("Artist created:", newArtist);
+  return newArtist;
+}
+
 //READ
 // Find all Files
 async function getAllFiles() {
   const files = await File.findAll();
-  console.log("All files:", files);
+  console.log("All files #:", files.length);
+  return files;
 }
 
 // Find all Tags
 async function getAllTags() {
   const tags = await Tag.findAll();
-  console.log("All tags:", tags);
+  console.log("All tags #:", tags.length);
   return tags;
 }
 
 // Find all Artistfunctions
 async function getAllArtistfunctions() {
   const artistfunctions = await Artistfunction.findAll();
-  console.log("All artistfunctions:", artistfunctions);
+  console.log("All artistfunctions #:", artistfunctions.length);
+  return artistfunctions;
 }
 
 // Find all Artists
 async function getAllArtists() {
   const artists = await Artist.findAll();
-  console.log("All arttists:", artists);
+  console.log("All artists #:", artists.length);
+  return artists;
 }
 
 // Find a File by ID
 async function getFileById(fileId) {
   const file = await File.findOne({ where: { fid: fileId } });
   console.log("File:", file);
+  return file;
 }
 
 // Find a Person by ID
 async function getPersonById(personId) {
   const person = await Person.findOne({ where: { aid: personId } });
   console.log("Person:", person);
+  return person;
 }
 
 // Find a Tag by ID
@@ -119,20 +161,53 @@ async function getTagById(tagId) {
   return tag;
 }
 
+// Find Artists by person and artistfunction
+// Return a list of all files with the given person and artistfunction
+async function getArtistFiles(person, artistfunction) {
+  const artistfiles = await Artist.findAll({
+    where: { person_id: person.pid, artistfunction_id: artistfunction.afid },
+  });
+  console.log("#ArtistFiles", artistfiles.length);
+  return artistfiles;
+}
+
 //UPDATE
+
+/* Update a File by ID
+async function updateFile(file) {
+  var updatedFile = await File.update(
+    {
+      title: file.getTitle(),
+      description: file.getDescription(),
+      reference: file.getReference(),
+      uri: file.getUri(),
+    },
+    { where: { fid: file.getId() } }
+  );
+  file.getTags().forEach(async (tag) => {
+    var dbtag = await getTagById(tag.getId());
+    updatedFile = await getFileById(file.getId());
+    console.log("Updated file: ", updatedFile);
+    await updatedFile.addTag(dbtag);
+  });
+  console.log("File updated:", updatedFile);
+  return updatedFile;
+}
+*/
 
 // Update a File by ID
 async function updateFile(file) {
-  const updatedFile = await File.update(
+  var updatedFile = await File.update(
     {
       title: file.title,
       description: file.description,
       reference: file.reference,
       uri: file.uri,
     },
-    { where: { fid: file.fid } }
+    { where: { fid: file.getId() } }
   );
   console.log("File updated:", updatedFile);
+  return updatedFile;
 }
 
 async function updatePerson(person) {
@@ -147,12 +222,23 @@ async function updatePerson(person) {
     { where: { aid: person.aid } }
   );
   console.log("Person updated:", updatedPerson);
+  return updatedPerson;
 }
-
+/*
 async function updateTag(tag) {
   const updatedTag = await Tag.update(
     { tagname: tag.getTagname() },
     { where: { tid: tag.getId() } }
+  );
+  //console.log("Tag updated:", updatedTag);
+  return await getTagById(tag.getId());
+}
+*/
+
+async function updateTag(tag) {
+  const updatedTag = await Tag.update(
+    { tagname: tag.tagname },
+    { where: { tid: tag.tid } }
   );
   console.log("Tag updated:", updatedTag);
   return await getTagById(tag.getId());
@@ -164,6 +250,34 @@ async function updateArtistfunction(artistfunction) {
     { where: { afid: artistfunction.afid } }
   );
   console.log("Artistfunction updated:", updatedArtistfunction);
+  return updatedArtistfunction;
+}
+
+//Filetags and Artisttags are not updated directly, but through the File and Artist objects
+
+async function updateFiletags(file, tags) {
+  console.log("update filetag file: ", tags);
+  const updatedFiletag = await file.setTags(tags);
+  console.log("Filetag updated:", updatedFiletag);
+  return updatedFiletag;
+}
+
+async function addArtist(person, artistfunction, file) {
+  const addedArtist = await createArtist(person, artistfunction, file);
+  console.log("Artist added:", addedArtist);
+  return addedArtist;
+}
+
+async function deleteArtist(person, artistfunction, file) {
+  const deletedArtist = await Artist.destroy({
+    where: {
+      file_id: file.fid,
+      person_id: person.aid,
+      artistfunction_id: artistfunction.afid,
+    },
+  });
+  console.log("Artist deleted:", deletedArtist);
+  return deletedArtist;
 }
 
 //DELETE
@@ -202,6 +316,7 @@ module.exports = {
   createTag,
   createPerson,
   createArtistFunction,
+  createArtist,
   getAllFiles,
   getAllTags,
   getAllArtistfunctions,
@@ -210,8 +325,11 @@ module.exports = {
   getPersonById,
   updateFile,
   updateTag,
+  updateFiletags,
   updatePerson,
   updateArtistfunction,
+  addArtist,
+  deleteArtist,
   deleteFile,
   deletePerson,
   deleteTag,
--- index.js
+++ index.js
@@ -1,24 +1,84 @@
 require("dotenv").config();
 
 const db = require("./Database/database");
-const Tag = require("./Model/Tag");
 
-testdb();
+initiateDatabase();
 
-async function testdb() {
-  var dbtags = await db.getAllTags();
-  //console.log("DbTag:", dbtags);
-  var tags = [];
-  dbtags.forEach((tag) => {
-    var newtag = new Tag();
-    newtag.setId(tag.tid);
-    newtag.setTagname(tag.tagname);
-    newtag.setCreatedAt(tag.createdAt);
-    newtag.setUpdatedAt(tag.updatedAt);
-    tags.push(newtag);
+async function initiateDatabase() {
+  await db.createDb();
+
+  var tag1 = await db.createTag({ tagname: "Classical" });
+  var tag2 = await db.createTag({ tagname: "Romantic" });
+  var tag3 = await db.createTag({ tagname: "Poetry" });
+
+  var af1 = await db.createArtistFunction({ artistfunction: "Composer" });
+  var af2 = await db.createArtistFunction({ artistfunction: "Writer" });
+
+  var p1 = await db.createPerson({
+    firstname: "Wolfgang",
+    firstnames: "Amadeus",
+    lastname: "Mozart",
+    date_of_birth: "1756-01-27",
+    date_of_death: "1791-12-05",
   });
-  console.log("Tag 3 :", tags[2].getTagname());
-  tags[2].setTagname("Comedy");
-  await Tag.writeTagToDb(tags[2]);
-  console.log("Tag written 2:", tags[2].getId());
+
+  var p2 = await db.createPerson({
+    firstname: "Franz",
+    firstnames: "",
+    lastname: "Schubert",
+    date_of_birth: "1797-01-31",
+    date_of_death: "1828-11-19",
+  });
+
+  var p3 = await db.createPerson({
+    firstname: "Frédérick",
+    firstnames: "",
+    lastname: "Chopin",
+    date_of_birth: "1810-03-01",
+    date_of_death: "1849-10-17",
+  });
+
+  var p4 = await db.createPerson({
+    firstname: "Johann",
+    firstnames: "",
+    lastname: "Goethe",
+    date_of_birth: "1749-08-28",
+    date_of_death: "1832-03-22",
+  });
+
+  var file1 = await db.createFile({
+    uri: "https://www.youtube.com/watch?v=6JQm5aSjX6g",
+    title: "Mozart - Symphony No. 40 in G minor, K. 550",
+    description:
+      "Symphony No. 40 in G minor, K. 550, was written by Wolfgang Amadeus Mozart in 1788. It is sometimes referred to as the Great G minor symphony, to distinguish it from the Little G minor symphony, No. 25. The two are the only extant minor key symphonies Mozart wrote.",
+    reference: "https://en.wikipedia.org/wiki/Symphony_No._40_(Mozart)",
+  });
+
+  var file2 = await db.createFile({
+    uri: "https://www.youtube.com/watch?v=ef-4Bv5Ng0w",
+    title: "Chopin - Nocturne in E-flat major, Op. 9, No. 2",
+    description:
+      "Nocturne in E-flat major, Op. 9, No. 2, is very possibly the most famous work ever penned by Frédéric Chopin. It is also one of the most popular nocturnes in the repertoire.",
+    reference: "https://en.wikipedia.org/wiki/Nocturnes,_Op._9_(Chopin)",
+  });
+
+  var file3 = await db.createFile({
+    uri: "https://www.youtube.com/watch?v=JS91p-vmSf0",
+    title: "Erlkönig",
+    description:
+      "Erlkönig is a poem by Johann Wolfgang von Goethe. It depicts the death of a child assailed by a supernatural being, the Erlking or 'Erlkönig'.",
+    reference: "https://en.wikipedia.org/wiki/Erlkönig_(Goethe)",
+  });
+
+  await db.updateFiletags(file1, [tag1]);
+  await db.updateFiletags(file2, [tag2]);
+  await db.updateFiletags(file3, [tag2, tag3]);
+
+  await db.createArtist(p2, af1, file1);
+  await db.createArtist(p2, af1, file2);
+  await db.createArtist(p3, af1, file3);
+  await db.createArtist(p4, af2, file3);
+
+  await db.deleteArtist(p2, af1, file1);
+  await db.addArtist(p1, af1, file1);
 }
